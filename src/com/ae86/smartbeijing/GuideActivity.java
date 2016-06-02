be146d387b4004da279ea34c3d70500d54d570bf
starae86@126.com <starae86@126.com>
2016-06-02 14:47:30 +0800

    填充引导界面

--- src/com/ae86/smartbeijing/GuideActivity.java
+++ src/com/ae86/smartbeijing/GuideActivity.java
@@ -1,13 +1,70 @@
 package com.ae86.smartbeijing;
 
+
+import java.util.ArrayList;
+
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageView;
 
 public class GuideActivity extends Activity{
+	
+	private ViewPager mViewPager;
+	private int[] mImageIds = new int[] { R.drawable.guide_1,
+			R.drawable.guide_2, R.drawable.guide_3 };
+	private ArrayList<ImageView> mImageViewList;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_guide);
-		findViewById(R.id.tv);
+		mViewPager = (ViewPager) findViewById(R.id.vp_guide);
+		initData();
+		mViewPager.setAdapter(new GuideAdapter());
+	}
+	
+	private void initData(){
+		mImageViewList=new ArrayList<ImageView>();
+		for (int i = 0; i < mImageIds.length; i++) {
+			ImageView view = new ImageView(this);
+			view.setBackgroundResource(mImageIds[i]);
+			mImageViewList.add(view);
+			
+		}
+	}
+	
+	
+ 	class GuideAdapter extends PagerAdapter{
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return mImageViewList.size();
+		}
+
+		@Override
+		public boolean isViewFromObject(View arg0, Object arg1) {
+			// TODO Auto-generated method stub
+			return arg0==arg1;
+		}
+		//初始化布局item
+		@Override
+		public Object instantiateItem(ViewGroup container, int position) {
+			ImageView view = mImageViewList.get(position);
+			container.addView(view);
+			return view;
+		}
+		//销毁item
+		@Override
+		public void destroyItem(ViewGroup container, int position, Object object) {
+			container.removeView((View) object);
+		}
 	}
 }
